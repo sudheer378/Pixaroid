fa00f9d63e4948e532c1bdb410220f9ae919b1ff
Sudheer pratap Singh <80664170+sudheer378@users.noreply.github.com>
2026-09-08 16:39:38 +0530

    Add Adsterra script to homepage build

---
+++ scripts/inject-adsterra-index.cjs
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const fs = require('node:fs');
+const path = require('node:path');
+
+const file = path.join(__dirname, '..', 'index.html');
+const TAG = '<script src="https://pl31242995.profitableratecpmnetwork.com/22/1e/c1/221ec1daebb5a9cce2d28b5964f3249b.js"></script>';
+const START = '<!-- PIXAROID_ADSTERRA_START -->';
+const END = '<!-- PIXAROID_ADSTERRA_END -->';
+
+if (!fs.existsSync(file)) throw new Error('index.html not found');
+
+let html = fs.readFileSync(file, 'utf8');
+if (!/<body\b[^>]*>/i.test(html)) throw new Error('index.html has no body element');
+
+const blockPattern = new RegExp(`${START.replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\$&')}[\\s\\S]*?${END.replace(/[.*+?^${}()|[\\]\\\\]/g, '\\\\$&')}`, 'gi');
+html = html.replace(blockPattern, '');
+html = html.split(TAG).join('');
+
+const block = `\n${START}\n${TAG}\n${END}\n`;
+html = html.replace(/(<body\b[^>]*>)/i, `$1${block}`);
+fs.writeFileSync(file, html, 'utf8');
+console.log('✓ Adsterra script installed once on index.html');
